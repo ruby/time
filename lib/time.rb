ruby/time

commit b8c50d236ccd20f8d6064d3c03b9f388af3dd1d1

Optimize `Time.xmlschema` by ~4x
It is currentyl fairly slow as it rely on a Regexp match, which
isn't very fast an allocates a lot.

Since Ruby 3.2, `Time.new(iso_string)` is supported, and over
8x faster than `Time.xmlschema`.

So if we use the regexp with `match?` to avoid the allocations,
and then delegate to `.new`, we can make `Time.xmlschema` around
4 times faster:

```
ruby 4.0.5 (2026-05-20 revision 64336ffd0e) +YJIT +PRISM [arm64-darwin25]
Warming up --------------------------------------
           xmlschema    67.645k i/100ms
            Time.new   531.202k i/100ms
                 opt   263.541k i/100ms
Calculating -------------------------------------
           xmlschema    680.426k (± 0.7%) i/s    (1.47 μs/i) -      3.450M in   5.070197s
            Time.new      5.736M (± 0.9%) i/s  (174.34 ns/i) -     28.685M in   5.000832s
                 opt      2.779M (± 1.1%) i/s  (359.78 ns/i) -     13.968M in   5.025252s

Comparison:
xmlschema:   680426.2 i/s
 Time.new:  5736027.1 i/s - 8.43x  faster
      opt:  2779497.0 i/s - 4.08x  faster
```

```ruby
require 'time'
require 'benchmark/ips'

class Time
  class << self
    def baseline(time)
      if /\A\s*
          (-?\d+)-(\d\d)-(\d\d)
          T
          (\d\d):(\d\d):(\d\d)
          (\.\d+)?
          (Z|[+-]\d\d(?::?\d\d)?)?
          \s*\z/ix =~ time
        year = $1.to_i
        mon = $2.to_i
        day = $3.to_i
        hour = $4.to_i
        min = $5.to_i
        sec = $6.to_i
        usec = 0
        if $7
          usec = Rational($7) * 1000000
        end
        if $8
          zone = $8
          off = zone_offset(zone)
          year, mon, day, hour, min, sec =
            apply_offset(year, mon, day, hour, min, sec, off)
          t = self.utc(year, mon, day, hour, min, sec, usec)
          force_zone!(t, zone, off)
          t
        else
          self.local(year, mon, day, hour, min, sec, usec)
        end
      else

lib/time.rb
@@ -664,31 +664,41 @@ class Time
 
     private
 
-    def _xmlschema(pattern, time)
-      if pattern =~ time
-        year = $1.to_i
-        mon = $2.to_i
-        day = $3.to_i
-        hour = $4.to_i
-        min = $5.to_i
-        sec = $6.to_i
-        usec = 0
-        if $7
-          usec = Rational($7) * 1000000
-        end
-        if $8
-          zone = $8
-          off = zone_offset(zone)
-          year, mon, day, hour, min, sec =
-            apply_offset(year, mon, day, hour, min, sec, off)
-          t = self.utc(year, mon, day, hour, min, sec, usec)
-          force_zone!(t, zone, off)
-          t
+    if RUBY_VERSION >= "3.2"
+      def _xmlschema(pattern, time)
+        if pattern.match?(time)
+          new(time)
         else
-          self.local(year, mon, day, hour, min, sec, usec)
+          raise ArgumentError.new("invalid xmlschema format: #{time.inspect}")
         end
-      else
-        raise ArgumentError.new("invalid xmlschema format: #{time.inspect}")
+      end
+    else
+      def _xmlschema(pattern, time)
+        if pattern =~ time
+          year = $1.to_i
+          mon = $2.to_i
+          day = $3.to_i
+          hour = $4.to_i
+          min = $5.to_i
+          sec = $6.to_i
+          usec = 0
+          if $7
+            usec = Rational($7) * 1000000
+          end
+          if $8
+            zone = $8
+            off = zone_offset(zone)
+            year, mon, day, hour, min, sec =
+              apply_offset(year, mon, day, hour, min, sec, off)
+            t = self.utc(year, mon, day, hour, min, sec, usec)
+            force_zone!(t, zone, off)
+            t
+          else
+            self.local(year, mon, day, hour, min, sec, usec)
+          end
+        else
+          raise ArgumentError.new("invalid xmlschema format: #{time.inspect}")
+        end
       end
     end
 

commit 24f885cb8aed5a6cbad83d55583b7ec5e9774cfe

Handle case insensitive match for Time.{iso8601,xmlschema,rfc3339}
The T and Z in the regular expressions are case insensitive, but
Time.new on Ruby 3.2+ does not support the lowercase versions.
Arguably, lower case t and z shouldn't be allowed, but they were
historically supported. Time.rfc3339 is a new method, so backwards
compatibility is not technically necessary, but the ActiveSupport
Time.rfc3339 method supports the lower case versions.

The optimization in b8c50d236ccd20f8d6064d3c03b9f388af3dd1d1 broke
backwards compatibility for lower case t and z, since it started
calling Time.new.

While I don't necessarily have a problem with breaking backwards
compatibility in order to improve standards conformance (depending
on the case), I think the decision should be deliberate and not
accidental due to an optimization.

The fix is trivial, and doesn't cause an additional allocation,
since strip already returns a fresh string.

lib/time.rb
@@ -670,7 +670,9 @@ class Time
     if RUBY_VERSION >= "3.2"
       def _xmlschema(pattern, time) # :nodoc:
         if pattern.match?(time)
-          new(time.strip)
+          time = time.strip
+          time.upcase!
+          new(time)
         else
           raise ArgumentError.new("invalid xmlschema format: #{time.inspect}")
         end

test/test_time.rb
@@ -162,6 +162,8 @@ class TestTimeExtension < Test::Unit::TestCase # :nodoc:
                  Time.__send__(method, "2000-03-04T23:00:00+03:00"))
     assert_equal(Time.utc(2000, 3, 4, 20, 0, 0),
                  Time.__send__(method, "2000-03-04T20:00:00Z"))
+    assert_equal(Time.utc(2000, 3, 4, 20, 0, 0),
+                 Time.__send__(method, "2000-03-04t20:00:00z"))
     assert_equal(Time.utc(2000, 1, 16, 12, 0, 0),
                  Time.__send__(method, "2000-01-16T12:00:00Z"))
     assert_equal(Time.utc(1999, 12, 31, 23, 0, 0),